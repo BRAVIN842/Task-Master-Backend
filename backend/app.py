4323e0be4705ee33f22c64ddc3cf7b27e8b571e4
Bravin <onsasebravin853@gmail.com>
2024-03-07 11:15:37 +0300

    initialization

--- backend/app.py
+++ backend/app.py
@@ -10,3 +10,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'super-secret'
+
+db.init_app(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+CORS(app)
