ce5390ae7efdfe45833cee6b97196561d48d0038
Bravin <onsasebravin853@gmail.com>
2024-03-07 11:11:34 +0300

    Task table

--- backend/models.py
+++ backend/models.py
@@ -13,3 +13,18 @@ class User(db.Model):
 
     tasks = db.relationship('Task', backref='user', lazy=True)
     comments = db.relationship('Comment', backref='user', lazy=True)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    deadline = db.Column(db.DateTime, nullable=False)
+    progress = db.Column(db.Integer, nullable=False, default=0)
+    priority = db.Column(db.String(20), nullable=False, default='Low')
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    group_leader_id = db.Column(db.Integer, db.ForeignKey('group_leader.id'))  # Add this line
+    users = db.relationship('User', backref='assigned_tasks', lazy=True)
+    
+    comments = db.relationship('Comment', backref='task', lazy=True)
