5c323811039f9f37505838a36d4b70ab60d1273c
Bravin <onsasebravin853@gmail.com>
2024-03-07 11:14:52 +0300

    app.py configurations

--- backend/app.py
+++ backend/app.py
@@ -5,3 +5,8 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from flask_cors import CORS
 from models import db, User, Task, Comment, GroupLeader
 from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'super-secret'
