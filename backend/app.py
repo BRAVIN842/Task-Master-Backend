dd717b25b1346600dd70145acf910c57645f33c6
Bravin <onsasebravin853@gmail.com>
2024-03-07 11:14:07 +0300

    app.py imports

---
+++ backend/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_cors import CORS
+from models import db, User, Task, Comment, GroupLeader
+from datetime import datetime
